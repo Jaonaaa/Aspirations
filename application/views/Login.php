94ce3880b2e0e29c38730b426d807ff98cd9478f
Jaonaaa <jaonapaulferdinah@gmail.com>
2023-03-04 13:00:38 +0300

    Initialisation

---
+++ application/views/Login.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+$this->load->helper("url");
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Welcome to CodeIgniter</title>
+</head>
+
+<body>
+
+    Hello dude
+</body>
+
+</html>
