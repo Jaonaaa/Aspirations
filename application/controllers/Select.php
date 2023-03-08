4c3302fb73363ba9faef728b029621b3584f6771
Ambinintsoa <randriambololonaisabelle@gmail.com>
2023-03-08 19:42:17 +0100

    select

--- application/controllers/Select.php
+++ application/controllers/Select.php
@@ -21,18 +21,12 @@ class Select extends CI_Controller
     {
         //$this->session->userdata("iduser")
         $data = $this->Tache->getAllTaches(1);
-        foreach ($data as $task) {
-            $task['sousTaches'] = $this->Tache->getsoustachebytache($task['idtache']);
-        }
         echo json_encode($data);
     }
     public function gettaskperday($date)
     {
         //$this->session->userdata("iduser")
         $data = $this->Tache->gettachePerJour(1,$date);
-        foreach ($data as $task) {
-            $task['sousTaches'] = $this->Tache->getsoustachebytache($task['idtache']);
-        }
         echo json_encode($data);
     }
 
